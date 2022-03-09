c93c23d39466001ddb4bbce929b0e95035348d06
Ammar <mohammadammarkhan2@gmail.com>
2022-03-09 19:43:56 +0500

    add Partail html file add delete function in show for deleting post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all.order("created_at DESC")
   end
@@ -17,13 +18,32 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title,:body)
-
+    params.require(:post).permit(:title, :body)
   end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
 end
